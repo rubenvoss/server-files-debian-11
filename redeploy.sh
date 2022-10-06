208d95773791c44539b96b014f5105b57ed0b7b2
Ruben Voß <bekertshop@gmail.com>
2022-10-06 23:21:05 +0200

    add docker compose to script

--- redeploy.sh
+++ redeploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker pull rubenvoss/rails-production:latest
 docker stop rails_production_server
-docker system prune -f
-docker run -d --name=rails_production_server rubenvoss/rails-production:latest
+docker compose down
+docker compose up --build
